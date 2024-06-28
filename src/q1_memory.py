270512573a31e6a848810e95d158df84badf5fd0
Daniel Hebel <dhebellobos@gmail.com>
2024-06-28 02:02:13 -0400

    Se agrega el desarrollo de Q1 con pruebas de tiempo en challenge.ipynb

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,40 @@
 from typing import List, Tuple
 from datetime import datetime
+from collections import Counter
+import json
+import memory_profiler
 
+#@memory_profiler.profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+        # Dictionary to store the dates and users
+    dates = Counter()
+    user_counts = {}
+    
+    with open(file_path, "r") as file:
+        for line in file:
+            tweet = json.loads(line)
+            # Get the date and username
+            date = tweet['date'].split('T')[0]
+            username = tweet["user"]["username"]
+            
+            # Update the count of tweets per date
+            dates[date] += 1
+            
+            # Update the count of tweets per user per date
+            if date not in user_counts:
+                user_counts[date] = Counter()
+            user_counts[date][username] += 1
+    
+    # Top 10 dates with most tweets
+    top_dates = dates.most_common(10)
+    top_dates_users = []
+    
+    # Get the user with the most posts per date
+    for date, _ in top_dates:
+        top_user = user_counts[date].most_common(1)[0][0]
+        top_dates_users.append((datetime.strptime(date, "%Y-%m-%d").date(), top_user))
+    
+    # Sort by date
+    top_dates_users.sort(key=lambda x: x[0], reverse=True)
+    
+    return top_dates_users
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,39 @@
 from typing import List, Tuple
 from datetime import datetime
+from collections import Counter, defaultdict
+from heapq import nlargest
+import memory_profiler
+import json
 
+#@memory_profiler.profile
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    # Dictionary to store the count of tweets per date
+    date_counts = Counter()
+    # Dictionary to store the count of tweets per user per date
+    user_counts_by_date = defaultdict(Counter)
+    
+    with open(file_path, "r") as file:
+        for line in file:
+            tweet = json.loads(line)
+            # Get the date and username
+            date = tweet['date'].split('T')[0]
+            username = tweet["user"]["username"]
+            
+            # Update counts
+            date_counts[date] += 1
+            user_counts_by_date[date][username] += 1
+    
+    # Find the top 10 dates with most tweets
+    top_dates = nlargest(10, date_counts.items(), key=lambda x: x[1])
+    
+    top_dates_users = []
+    
+    # Get the user with the most posts per date
+    for date, _ in top_dates:
+        top_user = user_counts_by_date[date].most_common(1)[0][0]
+        top_dates_users.append((datetime.strptime(date, "%Y-%m-%d").date(), top_user))
+    
+    # Sort by date
+    top_dates_users.sort(key=lambda x: x[0], reverse=True)
+    
+    return top_dates_users
